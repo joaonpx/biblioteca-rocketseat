1316d89354caaacf6f447f56e3a375a23cf024a6
Npx <cheyprivado@gmail.com>
2024-01-07 18:16:50 -0300

    🔨 Atualizando fetch script

--- js/fetch.js
+++ js/fetch.js
@@ -1,19 +1,3 @@
-// fetch("https://api.thecatapi.com/v1/images/search")
-//   .then((response) => {
-//     response
-//       .json()
-//       .then((data) => {
-//         let imgUrl = data[0].url
-
-//         // document.querySelector("#cat-img").src = imgUrl
-
-//         console.log("gato atualizado")
-//       })
-//       .catch((error) => console.log(error))
-//       .finally(() => console.log("finalizando"))
-//   })
-//   .catch((error) => console.log(error))
-
 fetch("https://api.github.com/users/joaonpx")
   .then((response) => response.json())
   .then((data) => fetch(data.repos_url))
