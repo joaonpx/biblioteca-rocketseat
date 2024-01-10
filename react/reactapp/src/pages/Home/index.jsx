a2826306b55c6dec8fd87123c062d3cc52ec6f10
Npx <cheyprivado@gmail.com>
2024-01-09 21:10:00 -0300

    🏗️ Atualizando estrutura do projeto

---
+++ react/reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+export function Home() {
+  return (
+    <>
+      <h1>Lista de compras</h1>
+      <input type="text" placeholder="Digite o nome do item..." />
+      <button type="button">Adicionar</button>
+    </>
+  )
+}
